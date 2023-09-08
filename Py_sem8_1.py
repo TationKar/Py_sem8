39fae509dabb1bfa0e40ce6dcd83b4f8021d07fa
Timur Karimov <tationkar@gmail.com>
2023-09-08 19:56:32 +0300

    next ver

--- Py_sem8_1.py
+++ Py_sem8_1.py
@@ -22,6 +22,7 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QIcon, QStandardItem, QStandardItemModel
+from random import randint
 
 from ui import Ui_MainWindow
 
@@ -30,12 +31,12 @@ def load():
         global phone_book
         phone_book = json.load(fh)
     print("Загрузка контактов выполнена успешно")
-    # return phone_book
 
 def save():
     with open("contact.json", "w", encoding="utf-8") as fh:
         fh.write(json.dumps(phone_book, ensure_ascii=False))
     print("Справочник сохранен")
+    
 
 try:
     load()
@@ -44,6 +45,68 @@ except:
                    "Тетя Песя": {"phone_numbers": [9998881444]}}]
     save()
 
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
+    def __init__(self):
+        super().__init__()
+        # self.build()
+        # так получаем сыылку на родительское окно
+        # self.parrent = self.parent()
+
+        layout = QVBoxLayout()
+        self.label = QLabel("Введите данные нового контакта")
+        layout.addWidget(self.label)
+        self.line0 = QLineEdit()
+        self.line0.setPlaceholderText("Введите имя")
+        layout.addWidget(self.line0)
+        self.line1 = QLineEdit()
+        self.line1.setPlaceholderText("Введите номер тел.1")
+        layout.addWidget(self.line1)
+        self.line2 = QLineEdit()
+        self.line2.setPlaceholderText("Введите номер тел.2")
+        layout.addWidget(self.line2)
+        self.line3 = QLineEdit()
+        self.line3.setPlaceholderText("Введите дату рождения")
+        layout.addWidget(self.line3)
+        self.line4 = QLineEdit()
+        self.line4.setPlaceholderText("Введите email")
+        layout.addWidget(self.line4)
+        self.save_but = QtWidgets.QPushButton()
+        self.save_but.setObjectName("sv_but")
+        self.save_but.setText("Сохранить данные")
+        layout.addWidget(self.save_but)
+        self.setLayout(layout)
+        self.save_but.clicked.connect(self.save_contact)
+        
+    def save_contact(self):
+        name_contact = self.line0.text()
+        self.line0.clear()
+        num_contact1 = self.line1.text()
+        self.line1.clear()
+        num_contact2 = self.line2.text()
+        self.line2.clear()
+        new_birth_day = self.line3.text()
+        self.line3.clear()
+        new_mail = self.line4.text()
+        self.line4.clear()
+        phone_book[name_contact] = {"phone_numbers": [num_contact1, num_contact2], "birth_day": new_birth_day, "email": new_mail}
+        save()
+
+        # if Phone_bk().:
+        #     Phone_bk().fill_table()
+        # else:
+        #     print("Error!!!!")
+        
+        self.hide()
+        # phone_bk = Phone_bk()
+
+        # self.parent.
+        
+
+
 
 class Phone_bk(QtWidgets.QMainWindow):
 
@@ -51,6 +114,7 @@ class Phone_bk(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(Phone_bk, self).__init__()
+        self.w = AnotherWindow()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.init_UI()
@@ -69,20 +133,7 @@ class Phone_bk(QtWidgets.QMainWindow):
         self.ui.table_contacts.horizontalHeader().setStretchLastSection(True)
         self.model = QStandardItemModel()
         self.ui.table_contacts.setModel(self.model)
-        # self.ui.table_contacts.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
-        # self.ui.table_contacts.setSelectionMode(QTableWidget.selectionChanged)
-        # self.ui.table_contacts.itemS
-
-    # def setup_connection(self): 
-        # connected function to item selection changed trigger of my_table
-        # self.ui.table_contacts.itemSelectionChanged.connect(self.action_report_in_view)
-    
-    def action_report_in_view(self, *arg):
-        values = []
-        for selected_item in self.my_table.selectedItems():
-            # create [item from col 0, item from col 1]
-            values.insert(selected_item.column(), selected_item.text())
-        print(values)   
+        
 
     def fill_table(self):
 
@@ -91,6 +142,7 @@ class Phone_bk(QtWidgets.QMainWindow):
         # Устанавливаем количество строк и столбцов в таблице
         model.setRowCount(len(phone_book))
         model.setColumnCount(4)
+
 
         row = 0
         temp_dict = {}
@@ -115,36 +167,37 @@ class Phone_bk(QtWidgets.QMainWindow):
         for column in range(4):
             model.setHeaderData(column, Qt.Orientation.Horizontal, header_labels[column])
     
-    def add_cont(self):
-        name_contact, ok = QInputDialog.getText(
-            self,
-            "Имя нового контакта",
-            "1 Введите имя",
-        )
-        num_contact1, ok = QInputDialog.getText(
-            self,
-            "Телефон1",
-            "2 Введите 1й номер:",
-        )
-        num_contact2, ok = QInputDialog.getText(
-            self,
-            "Телефон2",
-            "3 Введите 2й номер:",
-        )
-        new_birth_day, ok = QInputDialog.getText(
-            self,
-            "День рождения",
-            "4 Введите день рождение:",
-        )
-        new_mail, ok = QInputDialog.getText(
-            self,
-            "Электронная почта",
-            "5 Введите e-mail",
-        )
-        if ok:
-            phone_book[name_contact] = {"phone_numbers": [num_contact1, num_contact2], "birth_day": new_birth_day, "email": new_mail}
-            save()
-            self.fill_table()
+    # def add_cont(self):
+        # name_contact, ok = QInputDialog.getText(
+        #     self,
+        #     "Имя нового контакта",
+        #     "1 Введите имя",
+        # )
+        # num_contact1, ok = QInputDialog.getText(
+        #     self,
+        #     "Телефон1",
+        #     "2 Введите 1й номер:",
+        # )
+        # num_contact2, ok = QInputDialog.getText(
+        #     self,
+        #     "Телефон2",
+        #     "3 Введите 2й номер:",
+        # )
+        # new_birth_day, ok = QInputDialog.getText(
+        #     self,
+        #     "День рождения",
+        #     "4 Введите день рождение:",
+        # )
+        # new_mail, ok = QInputDialog.getText(
+        #     self,
+        #     "Электронная почта",
+        #     "5 Введите e-mail",
+        # )
+        # if ok:
+        #     phone_book[name_contact] = {"phone_numbers": [num_contact1, num_contact2], "birth_day": new_birth_day, "email": new_mail}
+
+        # save()
+        # self.fill_table()
 
     def del_contact(self):
         name_delete, ok = QInputDialog.getText(
@@ -163,70 +216,6 @@ class Phone_bk(QtWidgets.QMainWindow):
             save()
             load()
             self.fill_table()
-    
-    # def fnd_contact(self):
-    #     name_find, ok = QInputDialog.getText(
-    #         self,
-    #         "Поиск контакта",
-    #         "Введите имя искомого контакта",
-    #     )
-
-    #     if ok:
-    #         not_found = True
-    #         for contact in phone_book:
-    #             if name_find in contact:
-    #                 not_found = False
-    #                 text_c = f"Совпадение для {name_find} найдены в контакте\n\nИмя контакта {contact}\n"
-    #                 temp_dict = phone_book[contact]
-    #                 temp_key = "phone_numbers"
-    #                 text_c += f"Номер(а) телефона(ов) {temp_dict[temp_key]}\n"
-
-    #                 try:
-    #                     temp_key = "birth_day"
-    #                     text_c += f"День рождение: {temp_dict[temp_key]}\n"
-    #                 except:
-    #                     pass
-    #                 try:
-    #                     temp_key = "email"
-    #                     text_c += f"E-mail: {temp_dict[temp_key]}"
-    #                 except:
-    #                     pass
-
-    #                 dialog = QMessageBox(parent=self, text=text_c)
-    #                 dialog.setWindowTitle("Find Dialog")
-    #                 ret = dialog.exec()
-
-    #         if not_found:
-    #             dialog = QMessageBox(parent=self, text=f"Контакт ''{name_find}'' отсутствует в списке")
-    #             dialog.setWindowTitle("Find Dialog")
-    #             ret = dialog.exec()
-
-
-
-    #         # if name_find in phone_book:
-    #         #     text_c = f"Имя контакта {name_find}\n"
-    #         #     temp_dict = phone_book[name_find]
-    #         #     temp_key = "phone_numbers"
-    #         #     text_c += f"Номер(а) телефона(ов) {temp_dict[temp_key]}\n"
-
-    #         #     try:
-    #         #         temp_key = "birth_day"
-    #         #         text_c += f"День рождение: {temp_dict[temp_key]}\n"
-    #         #     except:
-    #         #         pass
-    #         #     try:
-    #         #         temp_key = "email"
-    #         #         text_c += f"E-mail: {temp_dict[temp_key]}"
-    #         #     except:
-    #         #         pass
-
-    #         #     dialog = QMessageBox(parent=self, text=text_c)
-    #         #     dialog.setWindowTitle("Find Dialog")
-    #         #     ret = dialog.exec()
-    #         # else:
-    #         #     dialog = QMessageBox(parent=self, text=f"Контакт ''{name_find}'' отсутствует в списке")
-    #         #     dialog.setWindowTitle("Find Dialog")
-    #         #     ret = dialog.exec()
 
     def red_contact(self):
         name_find_red, ok = QInputDialog.getText(
@@ -311,6 +300,7 @@ class Phone_bk(QtWidgets.QMainWindow):
             QtWidgets.QLineEdit.EchoMode.Normal,
             temp_mail
         )
+        
         if ok:
             del phone_book[name_find_red]
             phone_book[name_contact] = {"phone_numbers": [num_contact1, num_contact2], "birth_day": new_birth_day, "email": new_mail}
@@ -373,28 +363,21 @@ class Phone_bk(QtWidgets.QMainWindow):
             
             self.tmp_phone_book = {}
 
-    # def contextMenuEvent(self, pos):
-    #     if self.selectionModel().selection().indexes():
-    #         for i in self.selectionModel().selection().indexes():
-    #             row, column = i.row(), i.column()
-    #         menu = QtGui.QMenu()
-    #         openAction = menu.addAction("Open")
-    #         deleAction = menu.addAction("Delete")
-    #         renaAction = menu.addAction("Rename")
-    #         action = menu.exec_(self.mapToGlobal(pos))
-    #         if action ==openAction:
-    #             self.openAction(row, column)
-
-    # def openAction(self, row, column):
-    #     if self._slideShowWin:
-    #         self._slideShowWin.showImageByPath(self._twoDLst[row][column])
-    #         self._animateUpOpen()
+    def add_cont(self):
+        # try:
+        #     if self.w is None:
+        #         self.w = AnotherWindow()
+        # except:
+        #     self.w = AnotherWindow()
+        #     pass
+        # print("hlp_contact нажат")
+        self.w.show()
+        # self.fill_table()
+        
 
     # def deleteSelected(self):
     #     # TODO
     #     pass
-
-
 
 app = QtWidgets.QApplication([])
 application = Phone_bk()
